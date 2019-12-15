112b94d6539e6842d5631eddc76abcf0fdd2afeb
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-12-15 09:16:42 +0700

    repair effect in css and js.

--- script.js
+++ script.js
@@ -7,5 +7,9 @@ keys.forEach(key => {
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note);
     noteAudio.currentTime = 0;
-    noteAudio.play()
+    noteAudio.play();
+    key.classList.add('active');
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+    })
 }
